0897dd41416fb57fc52a5c394a1e89eadae1370d
nassy93 <nas939@hotmail.com>
2017-09-12 01:03:46 +0800

    Modified Phone to inherit Contact as a test. JUnit test passed

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    public String value = "";
+
+    public String toString() {
+        return value;
+    }
+
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+
+}
